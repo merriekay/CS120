5cb9118f5d6071608499a30bc3453a47dd04f27e
Meredith Moore <Meredith.Moore@drake.edu>
2024-04-03 10:09:43 -0500

    Create DestructuringDemo.js

---
+++ Day18/DestructuringDemo.js
@@ -0,0 +1,11 @@
+// Example 1: Destructuring arrays
+
+// Array with elements
+const numbers = [1, 2, 3, 4, 5];
+
+// Destructuring assignment
+const [first, second, ...rest] = numbers;
+
+console.log(first); // Output: 1
+console.log(second); // Output: 2
+console.log(rest); // Output: [3, 4, 5]
